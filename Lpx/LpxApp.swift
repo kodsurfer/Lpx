2895a549eab4de650653edfbd27c81dde0061e96
kodsurfer <kodsurfer@yandex.com>
2024-08-30 00:56:34 +0300

    Initial Commit

---
+++ Lpx/LpxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LpxApp.swift
+//  Lpx
+//
+//  Created by sol on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LpxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
